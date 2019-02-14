183e99af6e4923d1c116fff80562aa1835193455
Morgan Hartman <morganhartman@Morgans-MacBook-Air.local>
2019-02-14 13:02:18 -0700

    api

--- script.js
+++ script.js
@@ -13,14 +13,28 @@ document.getElementById('emailButton').addEventListener('click', function(event)
 })
 
 document.getElementById('logIn').addEventListener('click', function(event) {
-    var value = "";
     if (document.getElementById('input').innerText === "Email") {
-        value = document.getElementById('usernameEmailInput').value;
-        console.log(`Email Input is ${value}.`);
+        var emailAddress = document.getElementById('usernameEmailInput').value;
+        console.log(`Email Input is ${emailAddress}.`);
+        const accessKey = "81a42f979a60a5020baa7b1ec7c058e5";
+        const url = "http://apilayer.net/api/check?access_key=" + accessKey + "&email=" + emailAddress;
+        fetch(url)
+            .then(function(response) {
+                return response.json();
+            }).then(function(json) {
+                let results = "";
+                console.log(json);
+                if (json.score >= 0.8 || json.format_valid === true) {
+                    results += "<span class='valid'> Valid </span>";
+                } else {
+                    results += "<span class='invalid'>Invalid email address.</span>";
+                }
+                document.getElementById('validity').innerHTML = results;
+            });
+
     } else if (document.getElementById('input').innerText === "Username") {
         value  = document.getElementById('usernameEmailInput').value;
         console.log(`Username Input is ${value}.`);
-
     } else {
         //throw error message
         console.log("error!!")
