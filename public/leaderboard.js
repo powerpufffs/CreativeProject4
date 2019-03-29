89f4266aa79a337250d1bb89f5161431a869823a
Z <asterixandisaac@gmail.com>
2019-03-29 14:15:09 -0600

    final

--- public/leaderboard.js
+++ public/leaderboard.js
@@ -1,46 +1,23 @@
 var leaderboard = new Vue({
-    el: '#leaderboard',
-    data: {
-        items: [],
+  el: '#leaderboard',
+  data: {
+    items: [],
+  },
+  methods: {
+    async getGames() {
+      try {
+        let res = await axios.get("/api/games");
+        this.items = res.data;
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
     },
-<<<<<<< HEAD
     returnToGame: function () {
       window.location = 'http://localhost:2000/game.html';
     }
   },
-  computed: {
-
-  },
   created() {
     this.getGames();
   },
-
 });
-=======
-    methods: {
-        async getGames() {
-			try {
-				let res = await axios.get("/api/games");
-				this.items = res.data;
-				return true;
-			} catch (error) {
-				console.log(error);
-			}
-        }
-    },
-    computed: {
-        shortenedItems: function() {
-            if (this.items.length > 10) {
-                return;
-            } else {
-                var shortenedItemsArray = this.items;
-                return shortenedItemsArray;
-            }
-        }
-    },
-    created() {
-      this.getGames();
-    },
-  
-  });
->>>>>>> cd276328d72cfea54de121487df5e063cf2daa9f
